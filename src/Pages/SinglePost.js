9d4f31dc94812a2c253f51856f8037434592ce08
priyansh3731 <118509082+priyansh3731@users.noreply.github.com>
2023-07-10 04:59:26 -0700

    change some line

--- src/Pages/SinglePost.js
+++ src/Pages/SinglePost.js
@@ -1,11 +1,14 @@
 import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState ,useContext} from 'react'
 import { useParams , Link } from 'react-router-dom';
+import { BookmarkContext } from '../context/BookmarkContextProvider';
+
 import "./Explore.css";
 
 export const SinglePost = () => {
   const {postId} = useParams();
   const [SinglepostsData,setSinglePostsData]=useState();
+  const {setBookmarkpost} = useContext(BookmarkContext)
 
   const fetchData = async() => {
     try{
@@ -13,6 +16,37 @@ export const SinglePost = () => {
       setSinglePostsData(post)
     }
     catch(e){console.log(e)}
+  }
+
+
+  const onclickHandler1=async(event,_id)=>{
+    try {
+    
+      const res = await axios.post(`api/posts/like/${_id}`,{},{headers:{authorization:localStorage.getItem("token")}})
+      setSinglePostsData(res.data.posts)
+     
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const onclickHandler2=async(event,_id)=>{
+    try {
+      const res = await axios.post(`api/posts/dislike/${_id}`,{},{headers:{authorization:localStorage.getItem("token")}})
+      setSinglePostsData(res.data.posts)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const bookmarkPostHandler = async(event,id)=>{
+    try {
+        const res = await axios.post(`api/users/bookmark/${id}/`,{},{headers:{authorization:localStorage.getItem("token")}})
+      setBookmarkpost(res.data.bookmarks)
+      
+    } catch (error) {
+      console.log(error)
+    }
   }
 
 
@@ -31,14 +65,12 @@ export const SinglePost = () => {
     </div>
     {SinglepostsData?.content}
     <br></br>
-    <button className='like'>like</button>
-    <button className='like'>bookmark</button>
-    <button className='like'>unlike</button>
+    <button className='like' onClick={(event)=>onclickHandler1(event,SinglepostsData?._id)}>{SinglepostsData?.likes.likeCount}</button>
+                  <button className='like' onClick={(event)=>bookmarkPostHandler(event,SinglepostsData?._id)}>bookmark</button>
+                  <button className='like' onClick={(event)=>onclickHandler2(event,SinglepostsData?._id)}>dislike</button>
     <footer>
         <Link to={"/"} className='manu'>Explore</Link>
-        <Link className='manu'>treding</Link>
         <Link className='manu'>post</Link>
-        <Link className='manu'>like</Link>
         <Link className='manu'>bookmark</Link>
         <Link className='manu'>profile</Link>
     </footer>
